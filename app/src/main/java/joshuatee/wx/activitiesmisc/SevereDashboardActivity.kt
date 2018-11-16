6fd1a9442cbe6907ffcef4afa8274fa4deba4bcd
josh <joshua.tee@gmail.com>
2018-11-16 06:10:03 -0500

    55047

--- app/src/main/java/joshuatee/wx/activitiesmisc/SevereDashboardActivity.kt
+++ app/src/main/java/joshuatee/wx/activitiesmisc/SevereDashboardActivity.kt
@@ -24,7 +24,6 @@ package joshuatee.wx.activitiesmisc
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
-import android.os.AsyncTask
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -46,8 +45,11 @@ import joshuatee.wx.spc.UtilitySPC
 import joshuatee.wx.util.UtilityShare
 import joshuatee.wx.util.UtilityShortcut
 
+import kotlinx.coroutines.*
+
 class SevereDashboardActivity : BaseActivity() {
 
+    private val uiDispatcher: CoroutineDispatcher = Dispatchers.Main
     private val bmAl = mutableListOf<Bitmap>()
     private lateinit var linearLayout: LinearLayout
     private lateinit var contextg: Context
@@ -71,94 +73,88 @@ class SevereDashboardActivity : BaseActivity() {
     }
 
     private fun refreshDynamicContent() {
-        GetContent().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
+        getContent()
     }
 
-    @SuppressLint("StaticFieldLeak")
-    private inner class GetContent : AsyncTask<String, String, String>() {
+    private fun getContent() = GlobalScope.launch(uiDispatcher) {
 
-        internal val bitmapArrRep = mutableListOf<Bitmap>()
-        internal val snWat = SevereNotice(PolygonType.WATCH)
-        internal val snMcd = SevereNotice(PolygonType.MCD)
-        internal val snMpd = SevereNotice(PolygonType.MPD)
+        val bitmapArrRep = mutableListOf<Bitmap>()
+        val snWat = SevereNotice(PolygonType.WATCH)
+        val snMcd = SevereNotice(PolygonType.MCD)
+        val snMpd = SevereNotice(PolygonType.MPD)
 
-        override fun onPreExecute() {
-            linearLayout.removeAllViews()
-            val wTor = SevereWarning(PolygonType.TOR)
-            val wTst = SevereWarning(PolygonType.TST)
-            val wFfw = SevereWarning(PolygonType.FFW)
-            wTor.generateString(contextg, MyApplication.severeDashboardTor.valueGet())
-            wTst.generateString(contextg, MyApplication.severeDashboardTst.valueGet())
-            wFfw.generateString(contextg, MyApplication.severeDashboardFfw.valueGet())
-            if (wTor.count > 0) {
-                val objTor = ObjectCardText(contextg, wTor.text)
-                objTor.setOnClickListener(View.OnClickListener { tvWarnClicked(".*?Tornado Warning.*?") })
-                linearLayout.addView(objTor.card)
-            }
-            if (wTst.count > 0) {
-                val objTst = ObjectCardText(contextg, wTst.text)
-                objTst.setOnClickListener(View.OnClickListener { tvWarnClicked(".*?Severe Thunderstorm Warning.*?") })
-                linearLayout.addView(objTst.card)
-            }
-            if (wFfw.count > 0) {
-                val objFfw = ObjectCardText(contextg, wFfw.text)
-                objFfw.setOnClickListener(View.OnClickListener { tvWarnClicked(".*?Flash Flood Warning.*?") })
-                linearLayout.addView(objFfw.card)
-            }
+        linearLayout.removeAllViews()
+        val wTor = SevereWarning(PolygonType.TOR)
+        val wTst = SevereWarning(PolygonType.TST)
+        val wFfw = SevereWarning(PolygonType.FFW)
+        wTor.generateString(contextg, MyApplication.severeDashboardTor.valueGet())
+        wTst.generateString(contextg, MyApplication.severeDashboardTst.valueGet())
+        wFfw.generateString(contextg, MyApplication.severeDashboardFfw.valueGet())
+        if (wTor.count > 0) {
+            val objTor = ObjectCardText(contextg, wTor.text)
+            objTor.setOnClickListener(View.OnClickListener { tvWarnClicked(".*?Tornado Warning.*?") })
+            linearLayout.addView(objTor.card)
+        }
+        if (wTst.count > 0) {
+            val objTst = ObjectCardText(contextg, wTst.text)
+            objTst.setOnClickListener(View.OnClickListener { tvWarnClicked(".*?Severe Thunderstorm Warning.*?") })
+            linearLayout.addView(objTst.card)
+        }
+        if (wFfw.count > 0) {
+            val objFfw = ObjectCardText(contextg, wFfw.text)
+            objFfw.setOnClickListener(View.OnClickListener { tvWarnClicked(".*?Flash Flood Warning.*?") })
+            linearLayout.addView(objFfw.card)
         }
 
-        override fun doInBackground(vararg params: String): String {
+        withContext(Dispatchers.IO) {
             snMcd.getBitmaps(MyApplication.severeDashboardMcd.valueGet())
             snWat.getBitmaps(MyApplication.severeDashboardWat.valueGet())
             snMpd.getBitmaps(MyApplication.severeDashboardMpd.valueGet())
             bitmapArrRep.add((UtilitySPC.getStormReportsTodayUrl()).getImage())
-            return "Executed"
         }
 
-        override fun onPostExecute(result: String) {
-            if (bitmapArrRep.size > 0) {
-                bitmapArrRep.indices.forEach { it ->
-                    val card = ObjectCardImage(contextg, bitmapArrRep[it])
-                    card.setOnClickListener(View.OnClickListener { ObjectIntent(contextg, SPCStormReportsActivity::class.java, SPCStormReportsActivity.NO, arrayOf("today")) })
-                    linearLayout.addView(card.card)
-                }
+        if (bitmapArrRep.size > 0) {
+            bitmapArrRep.indices.forEach { it ->
+                val card = ObjectCardImage(contextg, bitmapArrRep[it])
+                card.setOnClickListener(View.OnClickListener { ObjectIntent(contextg, SPCStormReportsActivity::class.java, SPCStormReportsActivity.NO, arrayOf("today")) })
+                linearLayout.addView(card.card)
             }
-            listOf(snWat, snMcd, snMpd)
-                    .asSequence()
-                    .filter { it.bmAl.size > 0 }
-                    .forEach { severeNotice ->
-                        severeNotice.bmAl.indices.forEach { j ->
-                            val card = ObjectCardImage(contextg, severeNotice.bmAl[j])
-                            var cla: Class<*>? = null
-                            var claStr = ""
-                            val claArgStr = severeNotice.strList[j]
-                            when (severeNotice.type) {
-                                PolygonType.MCD -> {
-                                    cla = SPCMCDWShowActivity::class.java
-                                    claStr = SPCMCDWShowActivity.NO
-                                }
-                                PolygonType.WATCH -> {
-                                    cla = SPCMCDWShowActivity::class.java
-                                    claStr = SPCMCDWShowActivity.NO
-                                }
-                                PolygonType.MPD -> {
-                                    cla = SPCMCDWShowActivity::class.java
-                                    claStr = SPCMCDWShowActivity.NO
-                                }
-                                else -> {
-                                }
-                            }
-                            val cl = cla
-                            val clStr = claStr
-                            card.setOnClickListener(View.OnClickListener { ObjectIntent(contextg, cl!!, clStr, arrayOf(claArgStr, "", severeNotice.toString())) })
-                            linearLayout.addView(card.card)
-                        }
-                    }
-            bmAl.addAll(snWat.bmAl)
-            bmAl.addAll(snMcd.bmAl)
-            bmAl.addAll(snMpd.bmAl)
-            bmAl.addAll(bitmapArrRep)
         }
+        listOf(snWat, snMcd, snMpd)
+                .asSequence()
+                .filter { it.bmAl.size > 0 }
+                .forEach { severeNotice ->
+                    severeNotice.bmAl.indices.forEach { j ->
+                        val card = ObjectCardImage(contextg, severeNotice.bmAl[j])
+                        var cla: Class<*>? = null
+                        var claStr = ""
+                        val claArgStr = severeNotice.strList[j]
+                        when (severeNotice.type) {
+                            PolygonType.MCD -> {
+                                cla = SPCMCDWShowActivity::class.java
+                                claStr = SPCMCDWShowActivity.NO
+                            }
+                            PolygonType.WATCH -> {
+                                cla = SPCMCDWShowActivity::class.java
+                                claStr = SPCMCDWShowActivity.NO
+                            }
+                            PolygonType.MPD -> {
+                                cla = SPCMCDWShowActivity::class.java
+                                claStr = SPCMCDWShowActivity.NO
+                            }
+                            else -> {
+                            }
+                        }
+                        val cl = cla
+                        val clStr = claStr
+                        card.setOnClickListener(View.OnClickListener { ObjectIntent(contextg, cl!!, clStr, arrayOf(claArgStr, "", severeNotice.toString())) })
+                        linearLayout.addView(card.card)
+                    }
+                }
+        bmAl.addAll(snWat.bmAl)
+        bmAl.addAll(snMcd.bmAl)
+        bmAl.addAll(snMpd.bmAl)
+        bmAl.addAll(bitmapArrRep)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
