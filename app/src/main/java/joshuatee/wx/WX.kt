15d77d30eb2642474596bf7c024f659c18deecbe
Joshua Tee <joshua.tee@gmail.com>
2020-03-04 19:50:57 -0500

    release 55374 commit 2020/03/04 19:49

--- app/src/main/java/joshuatee/wx/WX.kt
+++ app/src/main/java/joshuatee/wx/WX.kt
@@ -99,7 +99,8 @@ class WX : CommonActionBarFragment() {
                         0
                 )
         )
-        // material 1.1.0
+        // material 1.1.0, since we are using .Bridge theme the below is not needed
+        // but left for reference
         //slidingTabLayout.setTabTextColors(-1711276033, Color.WHITE)
         //val a = slidingTabLayout.tabTextColors
         //UtilityLog.d("wx COLOR", a.toString()) // -13746343
