176b7f12e3cf92becd5181976bb45eb47776c75b
Joshua Tee <joshua.tee@gmail.com>
2020-04-19 16:17:04 -0400

    [REF] misc

--- app/src/main/java/joshuatee/wx/external/ExternalLine.kt
+++ app/src/main/java/joshuatee/wx/external/ExternalLine.kt
@@ -70,18 +70,22 @@ internal class ExternalLine(val start: ExternalPoint, val end: ExternalPoint) {
             start.x
         else
             end.x
+
         val minX = if (start.x < end.x)
             start.x
         else
             end.x
+
         val maxY = if (start.y > end.y)
             start.y
         else
             end.y
+
         val minY = if (start.y < end.y)
             start.y
         else
             end.y
+
         //return if (point.x >= minX && point.x <= maxX && point.y >= minY && point.y <= maxY) {
         return point.x in minX..maxX && point.y >= minY && point.y <= maxY
     }
