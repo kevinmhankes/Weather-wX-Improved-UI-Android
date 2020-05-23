7112b6496e423a3a58f20b1c2250c640f08b82ef
Joshua Tee <joshua.tee@gmail.com>
2020-05-23 10:34:04 -0400

    [REF] misc

--- app/src/main/java/joshuatee/wx/radarcolorpalettes/UtilityColorPaletteGeneric.kt
+++ app/src/main/java/joshuatee/wx/radarcolorpalettes/UtilityColorPaletteGeneric.kt
@@ -36,7 +36,7 @@ object UtilityColorPaletteGeneric {
         val lowerEnd: Int
         var prodOffset = 0.0
         var prodScale = 1.0
-        val objColormap = MyApplication.colorMap[colorMapProductCode]!!
+        val objectColorPalette = MyApplication.colorMap[colorMapProductCode]!!
         when (colorMapProductCode) {
             94 -> {
                 scale = 2
@@ -83,11 +83,11 @@ object UtilityColorPaletteGeneric {
                 lowerEnd = -32
             }
         }
-        objColormap.position(0)
-        val dbzAl = mutableListOf<Int>()
-        val rAl = mutableListOf<Int>()
-        val gAl = mutableListOf<Int>()
-        val bAl = mutableListOf<Int>()
+        objectColorPalette.position(0)
+        val dbzList = mutableListOf<Int>()
+        val redList = mutableListOf<Int>()
+        val greenList = mutableListOf<Int>()
+        val blueList = mutableListOf<Int>()
         var r = "0"
         var g = "0"
         var b = "0"
@@ -97,20 +97,20 @@ object UtilityColorPaletteGeneric {
                 val items = if (line.contains(",")) line.split(",") else line.split(" ")
                 if (items.size > 4) {
                     if (priorLineHas6) {
-                        dbzAl.add(((items[1].toDoubleOrNull() ?: 0.0) * prodScale + prodOffset - 1).toInt())
-                        rAl.add(r.toIntOrNull() ?: 0)
-                        gAl.add(g.toIntOrNull() ?: 0)
-                        bAl.add(b.toIntOrNull() ?: 0)
-                        dbzAl.add(((items[1].toDoubleOrNull() ?: 0.0) * prodScale + prodOffset).toInt())
-                        rAl.add(items[2].toIntOrNull() ?: 0)
-                        gAl.add(items[3].toIntOrNull() ?: 0)
-                        bAl.add(items[4].toIntOrNull() ?: 0)
+                        dbzList.add(((items[1].toDoubleOrNull() ?: 0.0) * prodScale + prodOffset - 1).toInt())
+                        redList.add(r.toIntOrNull() ?: 0)
+                        greenList.add(g.toIntOrNull() ?: 0)
+                        blueList.add(b.toIntOrNull() ?: 0)
+                        dbzList.add(((items[1].toDoubleOrNull() ?: 0.0) * prodScale + prodOffset).toInt())
+                        redList.add(items[2].toIntOrNull() ?: 0)
+                        greenList.add(items[3].toIntOrNull() ?: 0)
+                        blueList.add(items[4].toIntOrNull() ?: 0)
                         priorLineHas6 = false
                     } else {
-                        dbzAl.add(((items[1].toDoubleOrNull() ?: 0.0) * prodScale + prodOffset).toInt())
-                        rAl.add(items[2].toIntOrNull() ?: 0)
-                        gAl.add(items[3].toIntOrNull() ?: 0)
-                        bAl.add(items[4].toIntOrNull() ?: 0)
+                        dbzList.add(((items[1].toDoubleOrNull() ?: 0.0) * prodScale + prodOffset).toInt())
+                        redList.add(items[2].toIntOrNull() ?: 0)
+                        greenList.add(items[3].toIntOrNull() ?: 0)
+                        blueList.add(items[4].toIntOrNull() ?: 0)
                     }
                     if (items.size > 7) {
                         priorLineHas6 = true
@@ -124,52 +124,52 @@ object UtilityColorPaletteGeneric {
         if (colorMapProductCode == 161) {
             // pad first 16, think this is needed
             (0 until 10).forEach { _ ->
-                if (rAl.size > 0 && gAl.size > 0 && bAl.size > 0) {
-                    objColormap.putBytes(rAl[0].toByte(), gAl[0].toByte(), bAl[0].toByte())
+                if (redList.size > 0 && greenList.size > 0 && blueList.size > 0) {
+                    objectColorPalette.putBytes(redList[0].toByte(), greenList[0].toByte(), blueList[0].toByte())
                 }
             }
         }
         if (colorMapProductCode == 99 || colorMapProductCode == 135) {
             // first two levels are range folder per ICD
-            if (rAl.size > 0 && gAl.size > 0 && bAl.size > 0) {
-                objColormap.putBytes(rAl[0].toByte(), gAl[0].toByte(), bAl[0].toByte())
-                objColormap.putBytes(rAl[0].toByte(), gAl[0].toByte(), bAl[0].toByte())
+            if (redList.size > 0 && greenList.size > 0 && blueList.size > 0) {
+                objectColorPalette.putBytes(redList[0].toByte(), greenList[0].toByte(), blueList[0].toByte())
+                objectColorPalette.putBytes(redList[0].toByte(), greenList[0].toByte(), blueList[0].toByte())
             }
         }
-        if (rAl.size > 0 && gAl.size > 0 && bAl.size > 0) {
-            (lowerEnd until dbzAl[0]).forEach { _ ->
-                objColormap.putBytes(rAl[0].toByte(), gAl[0].toByte(), bAl[0].toByte())
+        if (redList.size > 0 && greenList.size > 0 && blueList.size > 0) {
+            (lowerEnd until dbzList[0]).forEach { _ ->
+                objectColorPalette.putBytes(redList[0].toByte(), greenList[0].toByte(), blueList[0].toByte())
                 if (scale == 2) { // 94 reflectivity
-                    objColormap.putBytes(rAl[0].toByte(), gAl[0].toByte(), bAl[0].toByte())
+                    objectColorPalette.putBytes(redList[0].toByte(), greenList[0].toByte(), blueList[0].toByte())
                 }
             }
         }
-        dbzAl.indices.forEach { index ->
-            if (index < dbzAl.lastIndex) {
-                val low = dbzAl[index]
-                val lowColor = Color.rgb(rAl[index], gAl[index], bAl[index])
-                val high = dbzAl[index + 1]
-                val highColor = Color.rgb(rAl[index + 1], gAl[index + 1], bAl[index + 1])
+        dbzList.indices.forEach { index ->
+            if (index < dbzList.lastIndex) {
+                val low = dbzList[index]
+                val lowColor = Color.rgb(redList[index], greenList[index], blueList[index])
+                val high = dbzList[index + 1]
+                val highColor = Color.rgb(redList[index + 1], greenList[index + 1], blueList[index + 1])
                 val diff = high - low
-                objColormap.putBytes(rAl[index].toByte(), gAl[index].toByte(), bAl[index].toByte())
+                objectColorPalette.putBytes(redList[index].toByte(), greenList[index].toByte(), blueList[index].toByte())
                 if (scale == 2) {
-                    objColormap.putBytes(rAl[index].toByte(), gAl[index].toByte(), bAl[index].toByte())
+                    objectColorPalette.putBytes(redList[index].toByte(), greenList[index].toByte(), blueList[index].toByte())
                 }
                 (1 until diff).forEach { j ->
                     if (scale == 1) {
                         val colorInt = UtilityNexradColors.interpolateColor(lowColor, highColor, j.toDouble() / (diff * scale).toDouble())
-                        objColormap.putInt(colorInt)
+                        objectColorPalette.putInt(colorInt)
                     } else if (scale == 2) {
                         val colorInt = UtilityNexradColors.interpolateColor(lowColor, highColor, (j * scale - 1).toDouble() / (diff * scale).toDouble())
                         val colorInt2 = UtilityNexradColors.interpolateColor(lowColor, highColor, (j * scale).toDouble() / (diff * scale).toDouble())
-                        objColormap.putInt(colorInt)
-                        objColormap.putInt(colorInt2)
+                        objectColorPalette.putInt(colorInt)
+                        objectColorPalette.putInt(colorInt2)
                     }
                 }
             } else {
-                objColormap.putBytes(rAl[index].toByte(), gAl[index].toByte(), bAl[index].toByte())
+                objectColorPalette.putBytes(redList[index].toByte(), greenList[index].toByte(), blueList[index].toByte())
                 if (scale == 2) {
-                    objColormap.putBytes(rAl[index].toByte(), gAl[index].toByte(), bAl[index].toByte())
+                    objectColorPalette.putBytes(redList[index].toByte(), greenList[index].toByte(), blueList[index].toByte())
                 }
             }
         }
