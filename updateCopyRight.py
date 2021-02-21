97daf9aa069b42ffdc76753962907b01df3b2564
Joshua Tee <joshua.tee@gmail.com>
2021-02-21 13:19:42 -0500

    misc

--- updateCopyRight.py
+++ updateCopyRight.py
@@ -14,9 +14,9 @@ parser.add_argument("-c", "--commit", help="commit changes", action="store_true"
 args = parser.parse_args()
 
 files: List[str] = glob("app/src/main/java/joshuatee/wx/*.kt"
-files += glob("app/src/main/java/joshuatee/wx/*/*.kt")
+files2: List[str] = glob("app/src/main/java/joshuatee/wx/*/*.kt")
 
-for f in files:
+for f in files + files2:
     data = open(f).read()
     data = data.replace("2016, 2017, 2018, 2019, 2020", "2016, 2017, 2018, 2019, 2020, 2021")
     fh = open(f, "w")
